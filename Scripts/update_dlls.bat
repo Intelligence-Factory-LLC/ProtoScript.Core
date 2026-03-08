0b605dad40d660c86b6a01a001af6445e148245e
Matt Furnari <mattfurnari@hotmail.com>
2026-03-08 15:06:41 +0000

    Update RooTrax.Utilities path references to C:\\dev\\RooTrax and align update_dlls scripts

--- Scripts/update_dlls.bat
+++ Scripts/update_dlls.bat
@@ -3,17 +3,18 @@ set LIB_ROOT=%ROOT%\lib\
 set DEPLOY_ROOT=%ROOT%\Deploy\
 set ONTOLOGY_DEPLOY=%ROOT%\..\ontology-core\Deploy\
 
-xcopy /y c:\RooTrax\RooTrax.Utilities\Deploy\BasicUtilities.dll %LIB_ROOT%*.*
-xcopy /y c:\RooTrax\RooTrax.Utilities\Deploy\WebAppUtilities.dll %LIB_ROOT%*.*
+xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\BasicUtilities.dll %LIB_ROOT%*.*
+xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\WebAppUtilities.dll %LIB_ROOT%*.*
 
-xcopy /y c:\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Cache.dll %LIB_ROOT%*.*
-xcopy /y c:\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.dll %LIB_ROOT%*.*
-xcopy /y c:\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.DB.dll %LIB_ROOT%*.*
+xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Cache.dll %LIB_ROOT%*.*
+xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.dll %LIB_ROOT%*.*
+xcopy /y c:\dev\RooTrax\RooTrax.Utilities\Deploy\RooTrax.Common.DB.dll %LIB_ROOT%*.*
 
 xcopy /y C:\dev\Buffaly.Development\Deploy\CSharp.dll %LIB_ROOT%*.*
 xcopy /y C:\dev\Buffaly.Development\Deploy\CSharp.Parsers.dll %LIB_ROOT%*.*
 
 if exist "%ONTOLOGY_DEPLOY%Ontology.dll" xcopy /y "%ONTOLOGY_DEPLOY%Ontology*.dll" "%DEPLOY_ROOT%*.*"
+
 
 
 
